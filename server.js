40d0af42082bb5d6358b0ab441aaac396da31d34
Pam Tholan <pamelatholan@yahoo.com>
2019-07-20 13:22:57 -0400

    updated links

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
   });
 
+  app.get("/api/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+  // Displays all characters
+  app.get("/api/waitlist", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  
+
+
+
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
